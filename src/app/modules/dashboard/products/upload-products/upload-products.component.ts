967363487db82543dcee2b39d20984f5ece191a0
josuehoenicka <josuee.vzla@gmail.com>
2024-02-04 15:11:52 -0300

    feat: upload product logic & styles

--- src/app/modules/dashboard/products/upload-products/upload-products.component.ts
+++ src/app/modules/dashboard/products/upload-products/upload-products.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Output, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Output,
+  ViewEncapsulation,
+} from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { toast } from 'src/app/common/enums/toast';
 
 @Component({
   selector: 'app-upload-products',
@@ -10,18 +17,21 @@ export class UploadProductsComponent {
   @Output() onCancelEvent: EventEmitter<true> = new EventEmitter<true>();
   @Output() onFileUploadedEvent: EventEmitter<File> = new EventEmitter<File>();
 
+  constructor(private messageService: MessageService) {}
+
   fileUploaded: boolean = false;
 
   onCancelChild() {
     this.onCancelEvent.emit(true);
   }
 
-  onSave(e?: any) {
-
-  }
+  onSave(e?: any) {}
 
   onSelectFileUploadChild(event: any) {
     console.error(event);
+    if (this.fileUploaded) {
+      this.showMSG(toast.warn, 'Warn', 'The product was changed');
+    }
     this.fileUploaded = true;
   }
 
@@ -29,5 +39,23 @@ export class UploadProductsComponent {
     console.error(event);
     const file: File = event.files[0];
     this.onFileUploadedEvent.emit(file);
+  }
+
+  onRemoveFile() {
+    this.showMSG(toast.success, 'Ok', 'The product was removed');
+    this.fileUploaded = false;
+  }
+
+  showMSG(
+    severityShow: 'success' | 'info' | 'warn' | 'error' | 'custom',
+    title: string,
+    detail: string
+  ) {
+    this.messageService.add({
+      severity: severityShow,
+      summary: title,
+      key: 'toast',
+      detail: detail,
+    });
   }
 }
